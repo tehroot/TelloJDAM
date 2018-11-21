6733f35f52a319617dfa5c5ed50089413f87070b
Mark Cardish <rewt@me-irl-4.local>
2018-11-21 03:15:37 -0500

    test pynmea2

---
+++ running-gps.py
@@ -0,0 +1,8 @@
+import serial
+
+response = ""
+while(response != "Received"):
+    ser = serial.Serial(port='/dev/ttyS1', baudrate=9600, timeout=100)
+    response = ser.readline().decode('ascii', errors='replace')
+    ser.cancel_read()
+    print(response)
