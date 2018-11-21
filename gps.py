87bdb4101878ce9740d819c7c979251cd8ca2ef2
Mark Cardish <rewt@me-irl-4.local>
2018-11-21 03:10:52 -0500

    test pynmea2

--- gps.py
+++ gps.py
@@ -5,8 +5,10 @@ response = ""
 while(response != "Received"):
     ser = serial.Serial(port='/dev/ttyS1', baudrate=9600, timeout=100)
     response = ser.readline().decode('ascii', errors='replace')
+    streamreader = pynmea2.NMEAStreamReader()
+    while 1:
+        data = input().read()
+        for msg in streamreader.next(data):
+            print(msg)
     ser.cancel_read()
-    msg = pynmea2.parse(response)
-    print(msg)
-    print(response.strip())
 
