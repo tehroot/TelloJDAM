0b4d45d101fe4d7e2f23840988fe069ccaab47f6
Mark Cardish <rewt@me-irl-4.local>
2018-11-21 02:46:57 -0500

    test pynmea2

--- gps.py
+++ gps.py
@@ -1,8 +1,11 @@
 import serial
+import pynmea2
 
 response = ""
 while(response != "Received"):
     ser = serial.Serial(port='/dev/ttyS1', baudrate=9600, timeout=100)
     response = ser.read(100)
     ser.cancel_read()
-    print(response)
+    msg = pynmea2.parse(response)
+    print(msg)
+
