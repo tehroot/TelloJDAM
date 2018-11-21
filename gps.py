af7d94fcfbce696f6d3947014cca2ecea8268a20
Mark Cardish <rewt@me-irl-4.local>
2018-11-21 01:22:57 -0500

    byte increase

--- gps.py
+++ gps.py
@@ -5,4 +5,4 @@ while(response != "Received"):
     ser = serial.Serial(port='/dev/ttyS1', baudrate=9600, timeout=100)
     response = ser.read(100)
     ser.cancel_read()
-    print(repr(response))
+    print(response)
