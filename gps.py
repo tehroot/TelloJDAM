7b7a83e5f9a2313af9e3e22172daaca7a0a7a6a1
Mark Cardish <rewt@me-irl-4.local>
2018-11-21 04:01:07 -0500

    maybe we're getting closer

--- gps.py
+++ gps.py
@@ -3,7 +3,7 @@ import pynmea2
 
 response = ""
 with serial.Serial('/dev/ttyS1', baudrate=9600, timeout=1) as ser:
-    for i in range(10):
+    while(respone != "finished"):
         line = ser.readline().decode('ascii', errors='replace')
         msg = pynmea2.parse(line.strip())
         print(msg)
