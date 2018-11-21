24059901218247ec4118b4770d31a62902f4e6bc
Mark Cardish <rewt@me-irl-4.local>
2018-11-21 04:02:42 -0500

    lol

--- gps.py
+++ gps.py
@@ -7,4 +7,3 @@ with serial.Serial('/dev/ttyS1', baudrate=9600, timeout=1) as ser:
         line = ser.readline().decode('ascii', errors='replace')
         msg = pynmea2.parse(line.strip())
         print(msg)
-        print(msg.lat)
