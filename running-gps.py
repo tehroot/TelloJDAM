84b30ebc281eec1ee75bdae9597ee7d640a80a93
Mark Cardish <rewt@me-irl-4.local>
2018-11-21 04:11:12 -0500

    removed test script

--- running-gps.py
+++
@@ -1,6 +0,0 @@
-import serial
-
-with serial.Serial('/dev/ttyS1', baudrate=9600, timeout=1) as ser:
-    for i in range(10):
-        line = ser.readline().decode('ascii', errors='replace')
-        print(line.strip())
