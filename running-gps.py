d83e193d0c73e2bf58a7391d1d69f9a360343e09
Mark Cardish <rewt@me-irl-4.local>
2018-11-21 03:44:22 -0500

    test pynmea2

--- running-gps.py
+++ running-gps.py
@@ -3,4 +3,4 @@ import serial
 with serial.Serial('/dev/ttyS1', baudrate=9600, timeout=1) as ser:
     for i in range(10):
         line = ser.readline().decode('ascii', errors='replace')
-        print(line.strip)
+        print(line.strip())
