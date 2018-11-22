6f7e1c03ac2a4080c61f3e1ffa09791d396b85f9
Mark Cardish <rewt@me-irl-4.local>
2018-11-21 19:55:27 -0500

    removed test script

--- gps.py
+++ gps.py
@@ -4,6 +4,5 @@ import pynmea2
 response = ""
 with serial.Serial('/dev/ttyS1', baudrate=9600, timeout=1) as ser:
     while(response != "finished"):
-        line = ser.readline().decode('ascii', errors='replace')
-        msg = pynmea2.parse(line.strip(), check=True)
+        msg = pynmea2.parse(ser.readline().decode('ascii', errors='replace'), check=True)
         print(msg)
