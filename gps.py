54fa6177b2942393a1dc09bae6f93f7ec8fb176b
Mark Cardish <rewt@me-irl-4.local>
2018-11-22 02:04:59 -0500

    Getting more closer I guess

--- gps.py
+++ gps.py
@@ -3,33 +3,31 @@ import pynmea2
 import sys
 import time
 
+
 def connect_serial():
-    while True:
-        try:
-            with serial.Serial('/dev/ttyS1', baudrate=9600, timeout=1) as ser:
-                line = ser.readline()
-                return line
-        except Exception as e:
-            sys.stderr.write("Error connecting to UART" % type(e).__name__, e)
-        except KeyboardInterrupt as e:
-            sys.stderr.write("Keyboard Interrupt")
+    try:
+        with serial.Serial('/dev/ttyS1', baudrate=9600, timeout=1) as ser:
+            line = ser.readline()
+            return line
+    except Exception as e:
+        sys.stderr.write("Error connecting to UART" % type(e).__name__, e)
+    except KeyboardInterrupt as e:
+        sys.stderr.write("Keyboard Interrupt")
 
 
 def get_gps(line):
-
-    while line:
-        try:
-            msg = pynmea2.parse(line)
-            if msg.sentence_type == 'GGA' and msg.gps_qual == 1:
-                print(msg.longitude)
-                print(msg.latitude)
-                line = ''
-            elif msg.sentence_type == 'GGA' and msg.gps_qual == 0:
-                print("...awaiting gps fix")
-        except KeyboardInterrupt:
-            sys.stderr.write("Keyboard Interrupt")
-        except Exception as e:
-            sys.stderr.write('Error parsing %s: %s\n' % (type(e).__name__, e))
+    try:
+        msg = pynmea2.parse(line)
+        if msg.sentence_type == 'GGA' and msg.gps_qual == 1:
+            print(msg.longitude)
+            print(msg.latitude)
+            line = ''
+        elif msg.sentence_type == 'GGA' and msg.gps_qual == 0:
+            print("...awaiting gps fix")
+    except KeyboardInterrupt:
+        sys.stderr.write("Keyboard Interrupt")
+    except Exception as e:
+        sys.stderr.write('Error parsing %s: %s\n' % (type(e).__name__, e))
 
 
 def main():
