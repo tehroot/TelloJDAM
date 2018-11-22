7023613a4c490190f3fbb88268f8fd89ca5b0aa4
Mark Cardish <rewt@me-irl-4.local>
2018-11-21 23:16:15 -0500

    can't get any data lol

--- gps.py
+++ gps.py
@@ -1,13 +1,27 @@
 import serial
 import pynmea2
+import sys
 
-response = ""
-with serial.Serial('/dev/ttyS1', baudrate=9600, timeout=1) as ser:
-    while(response != "finished"):
-        line = ser.readline().decode('ascii', errors='replace')
-        msg = pynmea2.parse(line)
-        if msg.sentence_type == 'GGA' and msg.gps_qual == 1:
-            print(msg.longitude)
-            print(msg.latitude)
-        elif msg.sentence_type == 'GGA' and msg.gps_qual == 0:
-            print("...awaiting gps fix")
+
+def connect_serial():
+    try:
+        while True:
+            with serial.Serial('/dev/ttyS1', baudrate=9600, timeout=1) as ser:
+                line = ser.readline().decode('ascii', errors='replace')
+                return line
+    except Exception as e:
+        sys.stderr.write("Error connecting to UART" % type(e).__name__, e)
+    except KeyboardInterrupt as e:
+        sys.stderr.write("Keyboard Interrupt")
+
+def get_gps(line):
+    msg = pynmea2.parse(line)
+    if msg.sentence_type == 'GGA' and msg.gps_qual == 1:
+        print(msg.longitude)
+        print(msg.latitude)
+    elif msg.sentence_type == 'GGA' and msg.gps_qual == 0:
+        print("...awaiting gps fix")
+
+def main():
+    message = connect_serial()
+    get_gps(message)
