a97692bc950efb07fae9716e48de60e44ccd2c5d
Mark Cardish <rewt@me-irl-4.local>
2018-11-21 20:38:11 -0500

    i hate python

--- gps.py
+++ gps.py
@@ -5,4 +5,4 @@ response = ""
 with serial.Serial('/dev/ttyS1', baudrate=9600, timeout=1) as ser:
     while(response != "finished"):
         msg = pynmea2.parse(ser.readline().decode('ascii', errors='replace'), check=True)
-        print(msg)
+        print(msg.fields)
