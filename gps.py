5c219f3481ded8872eee612d29a303352bc08f31
Mark Cardish <rewt@me-irl-4.local>
2018-11-21 22:25:29 -0500

    can't get any data lol

--- gps.py
+++ gps.py
@@ -7,6 +7,10 @@ with serial.Serial('/dev/ttyS1', baudrate=9600, timeout=1) as ser:
         line = ser.readline().decode('ascii', errors='replace')
         print(line)
         msg = pynmea2.parse(line)
-        if msg.sentence_type == 'GGA' and msg.fields.__contains__(msg.status == 'A'):
-            print(msg.longitude)
+        if msg.sentence_type == 'GGA':
+            #print(msg.longitude)
+            #print(msg.latitude)
+        if msg.sentence_type == 'RMC' and msg.fields.__contains__(msg.status == 'A'):
             print(msg.latitude)
+            print(msg.longitude)
+
