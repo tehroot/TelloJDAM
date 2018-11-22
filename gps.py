eb6183865c6119d636daf25e8fef339f5dc6f629
Mark Cardish <rewt@me-irl-4.local>
2018-11-21 22:41:48 -0500

    can't get any data lol

--- gps.py
+++ gps.py
@@ -5,10 +5,9 @@ response = ""
 with serial.Serial('/dev/ttyS1', baudrate=9600, timeout=1) as ser:
     while(response != "finished"):
         line = ser.readline().decode('ascii', errors='replace')
-        print(line)
         msg = pynmea2.parse(line)
         if msg.sentence_type == 'GGA' and msg.gps_qual == 1:
             print(msg.longitude)
             print(msg.latitude)
         elif msg.sentence_type == 'GGA' and msg.gps_qual == 0:
-            print("...awaiting gps fix)")
+            print("...awaiting gps fix")
