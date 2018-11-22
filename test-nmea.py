da77900bf32ad22bbb314647b0c945759de621cc
Mark Cardish <rewt@me-irl-4.local>
2018-11-21 22:03:42 -0500

    can't get any data lol

--- test-nmea.py
+++ test-nmea.py
@@ -1,6 +1,8 @@
 import pynmea2
 
-msg = '$GPGGA,024050.000,3957.7748,N,07511.8180,W,1,4,2.30,81.6,M,-33.9,M,,*54'
-line = pynmea2.parse(msg)
-print(line.latitude)
-print(line.longitude)
+line = '$GPGGA,024050.000,3957.7748,N,07511.8180,W,1,4,2.30,81.6,M,-33.9,M,,*54'
+msg = pynmea2.parse(line)
+print(msg.sentence_type)
+if msg.sentence_type == 'GGA':
+    print(msg.longitude)
+    print(msg.latitude)
